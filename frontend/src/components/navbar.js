37c5725781a0a63f2c8bd32a8da241b0b38a88c6
Josh Raimo <Joshraimo@gmail.com>
2024-02-22 12:51:07 -0500

    testing

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import ReactNavbar from 'react-bootstrap/Navbar';
 
-// Testing functionality
+// Testing functionality again
 
 // Here, we display our Navbar
 export default function Navbar() {
