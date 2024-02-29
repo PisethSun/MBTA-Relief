0c6647ac9ab87e544ec7c4da15b78308fe2fa9e9
Josh Raimo <Joshraimo@gmail.com>
2024-02-29 12:40:21 -0500

    Post routes + view model

---
+++ backend/server/routes/posts/post.createPost.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const newPostModel = require('../../models/postModel');
+const mongoose = require("mongoose");
+
+router.post("/posts/createPost", async (req, res) => {
+  const { id, username, content, imageId } = req.body;
+
+  const createNewPost = newPostModel({
+    userId: mongoose.Types.ObjectId(id),
+    username: username,
+    content: content,
+    imageId: imageId, 
+  });
+
+  try {
+    const response = await newPostModel.create(createNewPost);
+    res.json({ msg: 'Post created successfully' });
+  } catch (err) {
+    console.error('Error creating post:', err);
+    res.status(500).json({ error: 'Could not create post' });
+  }
+});
+
+module.exports = router;
---
+++ backend/server/routes/posts/post.getAllByUsername.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const newPostModel = require('../../models/postModel')
+
+router.get("/posts/getAllByUsername/:username", async (req, res) => {
+    const { username } = req.params
+    const allPosts = await newPostModel.find({ username: username })
+    return res.status(200).json(allPosts)
+})
+
+module.exports = router;
