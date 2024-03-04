f5268bf9d87ec91b53303ab68007a0c2ceeb7335
PisethSun <pisethsun92@gmail.com>
2024-03-04 01:04:20 -0500

    add table

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,54 +1,60 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import getUserInfo from '../../utilities/decodeJwt'
-
-
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import getUserInfo from '../../utilities/decodeJwt';
+import { Table, Button, Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
-    const [user, setUser] = useState({})
-    const navigate = useNavigate()
-    const handleClick = (e) => {
-        e.preventDefault();
-        localStorage.removeItem('accessToken')
-        return navigate('/')
-    }
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setUser(getUserInfo())
-    }, [])
+  const handleClick = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('accessToken');
+    return navigate('/');
+  };
 
-    
-    if (!user) return (
-        <div><h4>Log in to view this page.</h4></div>)
+  useEffect(() => {
+    setUser(getUserInfo());
+  }, []);
+
+  if (!user) return <div><h4>Log in to view this page.</h4></div>;
+
+  const { id, email, username, password } = user;
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h3>
+            Welcome to mbta Bathrooms reviews
+            <span className='username'> @{username}</span>
+          </h3>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <Table striped bordered hover>
+            <tbody>
+              
+              <tr>
+                <td>Email</td>
+                <td>{email}</td>
+              </tr>
+              <tr>
+                <td>Password</td>
+                <td>{password} (hashed)</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
         
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-        
-    const { id, email, username, password } = user
-    return (
-        <>
-            <div>
-                <h3>
-                    Welcome to mbta Bathrooms reviews
-                    <span className='username'> @{username}</span>
-                </h3>
-                <h3>
-                    Your userId in mongo db is
-                    <span className='userId'> {id}</span>
-                </h3>
-                <h3>
-                    Your registered email is
-                    <span className='email'> {email}</span>
-                </h3>
-                <h3>
-                    Your password is
-                    <span className='password'> {password} ( hashed )</span>
-                </h3>
-            </div>
-            <button onClick={(e) => handleClick(e)}>
-                Log Out
-            </button>
-        </>
-    )
-}
-
-export default HomePage
+export default HomePage;
