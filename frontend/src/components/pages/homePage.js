cfc626dbeba8a7324c65eca64f6523a8f9b5cee9
PisethSun <pisethsun92@gmail.com>
2024-03-05 12:22:47 -0500

    add google map

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import getUserInfo from '../../utilities/decodeJwt';
-import { Table, Button, Container, Row, Col } from 'react-bootstrap';
+import { Table, Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
   const [user, setUser] = useState({});
@@ -17,17 +17,20 @@ const HomePage = () => {
     setUser(getUserInfo());
   }, []);
 
-  if (!user) return <div><h4>Log in to view this page.</h4></div>;
+  if (!user) return <div><Row className="mt-3">
+  <Col>
+    <h4>MBTA-Releif Unofficial Bathroom </h4>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1p9qGlMr_bbXufuX5Uw0jXNuLLiuh-Un3&ehbc=2E312F" width="640" height="480"></iframe>
+  </Col>
+</Row></div>;
 
-  const { id, email, username, password } = user;
+  const { username, email } = user;
 
   return (
     <Container>
       <Row>
         <Col>
-          <h3>
-            Welcome
-          </h3>
+          <h3>Welcome</h3>
         </Col>
       </Row>
       <Row className="mt-3">
@@ -35,14 +38,13 @@ const HomePage = () => {
           <Table striped bordered hover>
             <tbody>
               <tr>
-                <td>User Name </td>
-                <td className='username'> {username}</td>
+                <td>User Name</td>
+                <td className='username'>{username}</td>
               </tr>
               <tr>
                 <td>Email</td>
                 <td>{email}</td>
               </tr>
-             
             </tbody>
           </Table>
         </Col>
