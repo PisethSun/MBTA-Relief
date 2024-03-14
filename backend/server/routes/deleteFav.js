9fcccc3d37e2d4f22d6c8dc8aa4b07b6f0b7a748
PisethSun <pisethsun92@gmail.com>
2024-03-13 22:25:49 -0400

    Deleted by ID , added messages

--- backend/server/routes/deleteFav.js
+++ backend/server/routes/deleteFav.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const  z = require('zod');
 const favoriteModel = require("../models/favoriteModel");
 
-router.put('/deleteFav/:favId', async (req, res) => {
+router.delete('/deleteFav/:favId', async (req, res) => {
     const id = req.params.favId
     try {
-        
        const fav = await favoriteModel.findByIdAndDelete(id);
-       res.json(fav)
+       res.json({message: `This Fav ID has been deleted ${id}`})
     } catch (error) {
         res.status(400).json({message: error.message})
     }
