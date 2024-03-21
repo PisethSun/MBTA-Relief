f47790ed4471e082f92f26b0a7f2b4a1df21851b
PisethSun <pisethsun92@gmail.com>
2024-03-21 12:55:49 -0400

    fix deleteAll , EditbyId- to worked

--- backend/server/routes/favoriteDeleteAll.js
+++ backend/server/routes/favoriteDeleteAll.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const favoriteModel = require("../models/favoriteModel");
+const favoriteModel = require('../models/favoriteModel');
 
 router.delete('/deleteAll', async (req, res) => {
-
     try {
-       const deleteAll= await favoriteModel.find();
-       const deleted = favoriteModel.deleteMany(deleteAll)
-       console.log(deleted);
-       res.json({message: `All Fav has been deleted`})
+        await favoriteModel.deleteMany();
+        return res.json(200).json({message: `All Fav has been deleted`})
     } catch (error) {
-
-        res.status(400).json({message: error.message})
+        console.error('Error deleting Fav:', error);
+        return res.status(500).json({ error: 'Failed to delete Favorite'});
     }
 
 });
