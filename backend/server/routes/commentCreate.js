8f05ee4b39c8c48eee7cdb13b924361d0bae7f77
Josh Raimo <Joshraimo@gmail.com>
2024-03-21 12:54:59 -0400

    Create comment fix

--- backend/server/routes/commentCreate.js
+++ backend/server/routes/commentCreate.js
@@ -4,18 +4,16 @@ const newPostModel = require('../models/commentModel');
 const mongoose = require("mongoose");
 
 router.post("/createComment", async (req, res) => {
-  const { commentId, username, comment, station  } = req.body;
-
-  const createNewComment = newPostModel({
-    commentId: commentId,
-    username: username,
-    comment: comment,
-    station: station, 
-  });
+  const { commentId, username, comment, station } = req.body;
 
   try {
-    const response = await newPostModel.create(createNewComment);
-    res.json({ msg: 'Comment created successfully' });
+    const response = await newPostModel.create({
+      commentId: commentId,
+      username: username,
+      comment: comment,
+      station: station,
+    });
+    res.json({ msg: 'Comment created successfully', commentId: response.id });
   } catch (err) {
     console.error('Error creating comment:', err);
     res.status(500).json({ error: 'Could not create comment' });
