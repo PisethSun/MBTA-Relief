13e39d91f352f6e9d8052f8a0e12a3444184cbc7
PisethSun <pisethsun92@gmail.com>
2024-04-10 02:35:07 -0400

    Dropdown works, and delete works.

--- frontend/src/components/pages/mbtaFavorite.js
+++ frontend/src/components/pages/mbtaFavorite.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Button, Form, Modal } from 'react-bootstrap';
 import axios from 'axios';
-import placeholder from './images/bathroomImg/placeholder.png'; 
+import placeholder from './images/bathroomImg/placeholder.png';
 
 function Alerts() {
   const [alerts, setAlerts] = useState([]);
-  const [editData, setEditData] = useState({ line: '', station: '' });
-  const [showEditModal, setShowEditModal] = useState(false);
   const [showCreateModal, setShowCreateModal] = useState(false);
   const [createData, setCreateData] = useState({ line: '', station: '' });
-  const [currentEditingId, setCurrentEditingId] = useState(null);
+  const [stations, setStations] = useState([]);
 
   useEffect(() => {
     fetchData();
+    fetchStations();
   }, []);
 
   async function fetchData() {
@@ -22,33 +21,31 @@ function Alerts() {
 
   const deleteFavorite = async (id) => {
     await axios.delete(`http://localhost:8081/favorite/deleteFav/${id}`);
-    fetchData();
+    fetchData(); // Reload the favorites to update the UI
+  };
+
+  const fetchStations = async () => {
+    try {
+      const response = await fetch("https://api-v3.mbta.com/stops?filter[route_type]=1");
+      const data = await response.json();
+      setStations(data.data.map((station) => ({
+        id: station.id,
+        name: station.attributes.name,
+      })));
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+    }
   };
 
   const deleteAllFavorites = async () => {
     try {
       await axios.delete('http://localhost:8081/favorite/deleteAll');
       alert('All favorites have been deleted successfully.');
-      window.location.reload();
+      fetchData();
     } catch (error) {
       console.error("There was an error deleting all favorites: ", error);
       alert('Failed to delete all favorites.');
-      window.location.reload();
     }
-  };
-
-  const showEditForm = (favorite) => {
-    setEditData({ line: favorite.line, station: favorite.station });
-    setCurrentEditingId(favorite._id);
-    setShowEditModal(true);
-  };
-
-  const handleEditInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
   };
 
   const handleCreateInputChange = (e) => {
@@ -59,12 +56,6 @@ function Alerts() {
     }));
   };
 
-  const saveEdit = async () => {
-    await axios.put(`http://localhost:8081/favorite/editFav/${currentEditingId}`, editData);
-    setShowEditModal(false);
-    fetchData();
-  };
-
   const saveNewFavorite = async () => {
     await axios.post('http://localhost:8081/favorite', { ...createData, userId: "65f1ec23429de3cae859ebe9" }, {
       headers: {
@@ -72,19 +63,17 @@ function Alerts() {
       },
     });
     setShowCreateModal(false);
-    fetchData();
+    fetchData(); // Reload the favorites to reflect the new addition
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
       <h1>Favorites</h1>
-      <br />
       <Button variant="success" onClick={() => setShowCreateModal(true)}>Create New Favorite</Button>
-      <br />
-      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginTop: '20px' }}>
         <Button
           variant="danger"
-          style={{ marginLeft: 'auto' }}
+          style={{ marginTop: '20px' }}
           onClick={() => {
             const isConfirmed = window.confirm("Are you sure you want to delete all favorites?");
             if (isConfirmed) {
@@ -96,59 +85,20 @@ function Alerts() {
           Delete All Favorites
         </Button>
       </div>
-      <br />
-      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginTop: '20px' }}>
         {alerts.map((alert) => (
-          <Card key={alert._id} className="mb-3" style={{ width: '18rem', border: '1px solid black', margin: '10px' }}>
+          <Card key={alert._id} className="mb-3" style={{ width: '18rem', margin: '10px' }}>
             <Card.Body>
               <Card.Img variant="top" src={placeholder} alt="Placeholder" />
               <Card.Text>
-                <strong>Station Line:</strong> {alert.station}<br />
-                <strong>Station Name:</strong> {alert.line}
+                <strong>Station Name:</strong> {alert.station}
               </Card.Text>
-              <Button variant="primary" onClick={() => showEditForm(alert)}>Edit</Button>
-              <Button variant="danger" onClick={() => {
-                const isConfirmed = window.confirm("Are you sure you want to delete?");
-                if (isConfirmed) {
-                  deleteFavorite(alert._id);
-                }
-              }}>Delete</Button>
+              <Button variant="danger" onClick={() => deleteFavorite(alert._id)}>Delete</Button>
             </Card.Body>
           </Card>
         ))}
       </div>
 
-      <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Favorite</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group>
-              <Form.Label>Line</Form.Label>
-              <Form.Control
-                type="text"
-                name="line"
-                value={editData.line}
-                onChange={handleEditInputChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Station</Form.Label>
-              <Form.Control
-                type="text"
-                name="station"
-                value={editData.station}
-                onChange={handleEditInputChange}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowEditModal(false)}>Close</Button>
-          <Button variant="primary" onClick={saveEdit}>Save Changes</Button>
-        </Modal.Footer>
-      </Modal>
       <Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>Create New Favorite</Modal.Title>
@@ -157,21 +107,18 @@ function Alerts() {
           <Form>
             <Form.Group>
               <Form.Label>Station Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="line"
-                value={createData.line}
-                onChange={handleCreateInputChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Station Line</Form.Label>
-              <Form.Control
-                type="text"
+              <Form.Select
                 name="station"
                 value={createData.station}
                 onChange={handleCreateInputChange}
-              />
+              >
+                <option value="">Select a station</option>
+                {stations.map((station) => (
+                  <option key={station.id} value={station.name}>
+                    {station.name}
+                  </option>
+                ))}
+              </Form.Select>
             </Form.Group>
           </Form>
         </Modal.Body>
